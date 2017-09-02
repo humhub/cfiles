4112cb69736de29f68e513680ccfba28d936974a
Carlos Ferreira <c.ferreira@acsiiweb.com>
2017-09-02 18:22:27 +0200

    Fix right-clic => Move action
    https://github.com/humhub/humhub-modules-cfiles/issues/49

--- views/move/modal_move.php
+++ views/move/modal_move.php
@@ -9,26 +9,29 @@ use humhub\widgets\ModalDialog;
 /* @var $model MoveForm */
 ?>
 
-<?php ModalDialog::begin(['header' => Yii::t('CfilesModule.base', '<strong>Move</strong> files'), 'size' => 'small']) ?>
+<?php ModalDialog::begin(['header' => Yii::t('CfilesModule.base', '<strong>Move</strong> files'), 'size' => 'small']); ?>
 
     <?php $form = ActiveForm::begin(); ?>
         <div class="modal-body">
-            <br />
+            <br>
 
-            <?= $this->render('directory_tree', ['root' => $model->root]) ?>
+            <?= $this->render('directory_tree', ['root' => $model->root]); ?>
 
-            <?= $form->field($model, 'destId')->hiddenInput(['id' => 'input-hidden-selectedFolder'])->label(false) ?>
+            <?= $form->field($model, 'destId')->hiddenInput(['id' => 'input-hidden-selectedFolder'])->label(false); ?>
 
-            <?php foreach ($model->selection as $index => $item) : ?>
-                <input class='input-hidden-selectedItem' type='hidden' name='selection[]' value='<?= $item ?>' />
-            <?php endforeach; ?>
+            <?php if (is_array($selectedItems)) {
+                foreach ($selectedItems as $index => $item) {
+                    echo "<input class='input-hidden-selectedItem' type='hidden' name='selected[]' value='$item'/>";
+                }
+            }
+            ?>
             
         </div>
 
         <div class="modal-footer">
-            <?= ModalButton::submitModal($model->getMoveUrl())?>
-            <?= ModalButton::cancel() ?>
+            <?= ModalButton::submitModal($model->getMoveUrl()); ?>
+            <?= ModalButton::cancel(); ?>
         </div>
-    <?php ActiveForm::end() ?>
+    <?php ActiveForm::end(); ?>
 
-<?php ModalDialog::end()?>
+<?php ModalDialog::end(); ?>
