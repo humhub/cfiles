f31d029a82bab8cf17fd4610033f53d0f7ffc952
HumHub Translations <info@humhub.com>
2020-10-30 18:01:38 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/models_FileSystemItem.php
+++ messages/fr/models_FileSystemItem.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'Is Public' => 'Est public',
-  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Note : Modifie la visibilité des dossiers, qui va être héritée par tous les fichiers et dossiers qu\'ils contiennent.',
+  'Is Public' => 'est public',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Note : modifie la visibilité des dossiers, qui va être héritée par tous les fichiers et dossiers qu\'ils contiennent.',
 );
