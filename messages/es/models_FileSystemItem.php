ec3624ea76e56feba84746c5e9ec944ad216be5e
HumHub Translations <info@humhub.com>
2021-02-06 18:30:52 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/models_FileSystemItem.php
+++ messages/es/models_FileSystemItem.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Is Public' => 'Es público',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Nota: Los cambios de visibilidad de las carpetas, serán incorporados por todos los archivos y carpetas contenidos.',
-    'Downloads' => '',
-];
+return array (
+  'Downloads' => 'Descargas',
+  'Is Public' => 'Es público',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Nota: Los cambios de visibilidad de las carpetas, serán incorporados por todos los archivos y carpetas contenidos.',
+);
--- messages/hu/models_FileSystemItem.php
+++ messages/hu/models_FileSystemItem.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Is Public' => 'Nyilvános',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Megjegyzés: a mappa láthatóságának megváltoztatása a benne lévő összes fájlra és mappára is érvényes.',
-    'Downloads' => '',
-];
+return array (
+  'Downloads' => 'Letöltések',
+  'Is Public' => 'Nyilvános',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Megjegyzés: a mappa láthatóságának megváltoztatása a benne lévő összes fájlra és mappára is érvényes.',
+);
