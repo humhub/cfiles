f9f29cf5cd4290c57ddd9f0c562e5cb99fc9091c
HumHub Translations <info@humhub.com>
2021-05-18 20:26:19 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/models_FileSystemItem.php
+++ messages/pl/models_FileSystemItem.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Is Public' => 'Jest Publiczny',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Notatka: Zmiany widoczności folderów, będą dziedziczone przez wszystkie zawarte pliki i foldery.',
-    'Downloads' => '',
-];
+return array (
+  'Downloads' => 'Pobrane',
+  'Is Public' => 'Jest Publiczny',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Notatka: Zmiany widoczności folderów, będą dziedziczone przez wszystkie zawarte pliki i foldery.',
+);
