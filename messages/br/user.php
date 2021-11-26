484fbc8f2c0b4acb96d33bc44c467beea499658f
HumHub Translations <info@humhub.com>
2021-11-26 14:51:40 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/br/user.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Are you really sure to delete this version?' => '',
+    'The version "{versionDate}" could not be deleted!' => '',
+    'The version "{versionDate}" has been deleted.' => '',
+];
