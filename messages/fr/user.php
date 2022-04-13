8e17f1d01314d7d980b0d3060f465e7c0abed43c
HumHub Translations <info@humhub.com>
2022-04-13 21:25:58 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/user.php
+++ messages/fr/user.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Are you really sure to delete this version?' => '',
-    'The version "{versionDate}" could not be deleted!' => '',
-    'The version "{versionDate}" has been deleted.' => '',
-];
+return array (
+  'Are you really sure to delete this version?' => 'Êtes-vous sûr·e de supprimer cette version ?',
+  'The version "{versionDate}" could not be deleted!' => 'La version "{versionDate}" n’a pas pu être supprimée !',
+  'The version "{versionDate}" has been deleted.' => 'La version "{versionDate}" a été supprimée.',
+);
