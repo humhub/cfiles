f5f7ec549006174810d91944431164ee6084a157
HumHub Translations <hello@humhub.com>
2022-06-20 16:44:48 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/models_FileSystemItem.php
+++ messages/ja/models_FileSystemItem.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Downloads' => '',
-    'Is Public' => '',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
-];
+return array (
+  'Downloads' => 'ダウンロード',
+  'Is Public' => '公開中',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
+);
