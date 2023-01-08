497e6fd13352491b95edc16fa51269400a5d799e
HumHub Translations <hello@humhub.com>
2023-01-08 15:46:07 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/user.php
+++ messages/hu/user.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Are you really sure to delete this version?' => '',
-    'The version "{versionDate}" could not be deleted!' => '',
-    'The version "{versionDate}" has been deleted.' => '',
-];
+return array (
+  'Are you really sure to delete this version?' => 'Biztos, hogy törli ezt a verziót?',
+  'The version "{versionDate}" could not be deleted!' => '"{versionDate}" verziót nem sikerült törölni!',
+  'The version "{versionDate}" has been deleted.' => '"{versionDate}” verzió törölve.',
+);
