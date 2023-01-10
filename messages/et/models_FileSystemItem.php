bb6a38cfe26544412e01f141e1330fe57c4a1816
HumHub Translations <hello@humhub.com>
2023-01-10 18:28:39 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/et/models_FileSystemItem.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Downloads' => '',
+    'Is Public' => '',
+    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
+];
---
+++ messages/et/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Add files' => '',
+    'Allows the user to modify or delete any files.' => '',
+    'Allows the user to upload new files and create folders' => '',
+    'Manage files' => '',
+];
