59aee944a8fea7e7137d557ed70925b68ad758d7
HumHub Translations <hello@humhub.com>
2023-04-28 18:25:57 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/permissions.php
+++ messages/ja/permissions.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Add files' => '追加ファイル',
-  'Allows the user to modify or delete any files.' => 'ユーザーが任意のファイルを変更または削除できるようにします。',
-  'Allows the user to upload new files and create folders' => 'ユーザーが新しいファイルをアップロードしてフォルダを作成できるようにします',
+  'Allows the user to modify or delete any files.' => '任意のファイルを変更または削除できるようにします。',
+  'Allows the user to upload new files and create folders' => '新しいファイルをアップロードしてフォルダを作成できるようにします',
   'Manage files' => 'ファイルの管理',
 );
