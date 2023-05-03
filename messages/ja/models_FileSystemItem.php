52aba1ef575703c6ac67bbf3ea59912c48048ebf
HumHub Translations <hello@humhub.com>
2023-05-03 16:59:32 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/models_FileSystemItem.php
+++ messages/ja/models_FileSystemItem.php
@@ -2,5 +2,5 @@
 return array (
   'Downloads' => 'ダウンロード',
   'Is Public' => '公開中',
-  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '注：フォルダーの可視性の変更は含まれている全てのファイルとフォルダーに継承されます。',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '注：フォルダーの可視性の変更は含まれているすべてのファイルとフォルダーに継承されます。',
 );
