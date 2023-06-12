32af99071486812beac2ed45b814e0c6b0aefab1
HumHub Translations <hello@humhub.com>
2023-06-12 09:17:23 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/models_FileSystemItem.php
+++ messages/nl/models_FileSystemItem.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Downloads' => 'Downloads',
-    'Is Public' => 'Is openbaar',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Opmerking: Wijzigingen in de zichtbaarheid wijzigen van de mappen worden ook toegepast op alle bestanden en mappen in de mappen.',
-    'Hide in Stream' => '',
-];
+return array (
+  'Downloads' => 'Downloads',
+  'Hide in Stream' => 'Verberg in nieuwsoverzicht.',
+  'Is Public' => 'Is openbaar',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Opmerking: Wijzigingen in de zichtbaarheid wijzigen van de mappen worden ook toegepast op alle bestanden en mappen hierin.',
+);
