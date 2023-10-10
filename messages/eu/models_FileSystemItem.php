1c1a4020afbdb6242e772a8c609f4b839953654a
HumHub Translations <hello@humhub.com>
2023-10-10 07:10:20 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/eu/models_FileSystemItem.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Downloads' => '',
+    'Hide in Stream' => '',
+    'Is Public' => '',
+    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
+];
