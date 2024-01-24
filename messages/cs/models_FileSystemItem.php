b7ea266374b97b4a5190c87bf6d302b14eb3520c
HumHub Translations <hello@humhub.com>
2024-01-24 19:37:46 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/cs/models_FileSystemItem.php
+++ messages/cs/models_FileSystemItem.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Downloads' => '',
-    'Hide in Stream' => '',
-    'Is Public' => '',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
-];
+return array (
+  'Downloads' => '',
+  'Hide in Stream' => '',
+  'Is Public' => 'Je veřejný',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => '',
+);
