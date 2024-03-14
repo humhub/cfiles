fdab98f08f8d66e2e117c969896e7fac758b7042
Yuriy Bakhtin <yurybakh@gmail.com>
2024-03-14 14:57:50 +0100

    Improve container module settings

--- controllers/ConfigContainerController.php
+++ controllers/ConfigContainerController.php
@@ -4,10 +4,19 @@ namespace humhub\modules\cfiles\controllers;
 
 use humhub\modules\cfiles\models\ConfigureContainerForm;
 use humhub\modules\content\components\ContentContainerController;
+use humhub\modules\content\components\ContentContainerControllerAccess;
+use humhub\modules\space\models\Space;
 use Yii;
 
 class ConfigContainerController extends ContentContainerController
 {
+    /**
+     * @inheritdoc
+     */
+    protected function getAccessRules()
+    {
+        return [[ContentContainerControllerAccess::RULE_USER_GROUP_ONLY => [Space::USERGROUP_ADMIN]]];
+    }
 
     public function actionIndex()
     {
@@ -20,5 +29,3 @@ class ConfigContainerController extends ContentContainerController
         return $this->render('index', ['model' => $form]);
     }
 }
-
-?>
