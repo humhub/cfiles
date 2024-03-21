56fbcd50d462458d03029c4c2823bff43f0c944c
HumHub Translations <hello@humhub.com>
2024-03-21 17:58:34 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/models_FileSystemItem.php
+++ messages/es/models_FileSystemItem.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Downloads' => 'Descargas',
-    'Is Public' => 'Es público',
-    'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Nota: Los cambios de visibilidad de las carpetas, serán incorporados por todos los archivos y carpetas contenidos.',
-    'Hide in Stream' => '',
-];
+return array (
+  'Downloads' => 'Descargas',
+  'Hide in Stream' => 'Ocultar en la línea de tiempo',
+  'Is Public' => 'Es público',
+  'Note: Changes of the folders visibility, will be inherited by all contained files and folders.' => 'Nota: Los cambios de visibilidad de las carpetas, serán incorporados por todos los archivos y carpetas contenidos.',
+);
