d17d38703340020f8acd3a4df0ddf221d23f3a2d
Marc Farré <contact@marc.fun>
2024-07-28 22:36:03 +0200

    Fix: Add autofocus on file or folder edit (for HumHub 1.17 - see https://github.com/humhub/humhub/issues/7136)

--- views/edit/modal_edit_file.php
+++ views/edit/modal_edit_file.php
@@ -15,25 +15,25 @@ use yii\bootstrap\ActiveForm;
 ?>
 
 <?php ModalDialog::begin([
-    'header' =>  Yii::t('CfilesModule.base', '<strong>Edit</strong> file'),
+    'header' => Yii::t('CfilesModule.base', '<strong>Edit</strong> file'),
     'animation' => 'fadeIn',
     'size' => 'small']) ?>
 
-    <?php $form = ActiveForm::begin(); ?>
+<?php $form = ActiveForm::begin() ?>
 
-        <div class="modal-body">
-            <?= $form->field($file->baseFile, 'file_name'); ?>
-            <?= $form->field($file, 'description')->widget(RichTextField::class); ?>
-            <?= $form->field($file, 'topics')->widget(TopicPicker::class, ['contentContainer' => $file->content->container])->label(false); ?>
-            <?= $form->field($file, 'visibility')->widget(ContentVisibilitySelect::class, ['readonly' => $file->parentFolder->content->isPrivate()]); ?>
-            <?= $form->field($file, 'hidden')->widget(ContentHiddenCheckbox::class, []); ?>
-            <?= $form->field($file, 'download_count')->staticControl(['style' => 'display:inline']); ?>
-        </div>
+<div class="modal-body">
+    <?= $form->field($file->baseFile, 'file_name')->textInput(['autofocus' => '']) ?>
+    <?= $form->field($file, 'description')->widget(RichTextField::class) ?>
+    <?= $form->field($file, 'topics')->widget(TopicPicker::class, ['contentContainer' => $file->content->container])->label(false) ?>
+    <?= $form->field($file, 'visibility')->widget(ContentVisibilitySelect::class, ['readonly' => $file->parentFolder->content->isPrivate()]) ?>
+    <?= $form->field($file, 'hidden')->widget(ContentHiddenCheckbox::class, []) ?>
+    <?= $form->field($file, 'download_count')->staticControl(['style' => 'display:inline']) ?>
+</div>
 
-        <div class="modal-footer">
-            <?= ModalButton::cancel() ?>
-            <?= ModalButton::submitModal($submitUrl) ?>
-        </div>
-    <?php ActiveForm::end() ?>
+<div class="modal-footer">
+    <?= ModalButton::cancel() ?>
+    <?= ModalButton::submitModal($submitUrl) ?>
+</div>
+<?php ActiveForm::end() ?>
 
 <?php ModalDialog::end() ?>
